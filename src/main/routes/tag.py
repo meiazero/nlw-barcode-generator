fd74915184b7bce8c0dbd69ebf83a95125ea6399
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-08 18:50:38 -0300

    feat: implement validate and error handler

--- src/main/routes/tag.py
+++ src/main/routes/tag.py
@@ -2,14 +2,24 @@ from flask import Blueprint, request, jsonify
 from src.dto.http_request import HttpRequest
 from src.dto.tag_creator import TagCreator
 
+from src.errors.error_handler import handle_errors
+
+from src.validators.tag_creator import tag_creator_validator
+
 tag = Blueprint('tag', __name__)
 
 
 @tag.route('/tag/generate', methods=['POST'])
 def generate_tag():
-    tag_creator = TagCreator()
+    response = None
 
-    http_request = HttpRequest(body=request.json)
-    response = tag_creator.validate_and_create_tag(http_request)
+    try:
+        tag_creator_validator(request)
+        tag_creator = TagCreator()
+
+        http_request = HttpRequest(body=request.json)
+        response = tag_creator.validate_and_create_tag(http_request)
+    except Exception as error:
+        response = handle_errors(error)
 
     return jsonify(response.body), response.status_code
